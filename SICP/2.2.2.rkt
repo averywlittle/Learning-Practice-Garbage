1aca85833615a64d61de0ea8ab840c0f637404c6
Avery Little <averywlittle@gmail.com>
2020-10-06 23:07:13 -0400

    page 152

--- SICP/2.2.2.rkt
+++ SICP/2.2.2.rkt
@@ -35,14 +35,51 @@
 
 ; Exercise 2.28
 ; Create a procedure called fringe that takes a tree and returns a list of all the leaves of the tree
+; I modified the count leaves algorithm, since that one visits every leaf and that's what is needed
+; Only, instead of counting, we append. And because append requires lists, we return the leaves as lists with only 1 item,
+; and we return null pointeres as empty lists!
+; This works with any list structure (binary, unbalanced, etc.)
 (define (fringe tree)
-  (define (iter t result)
-    (cond ((null? t) result)
-          ((not (pair? (car t)))
-           (iter (cdr t) (append t result)))
-          (else
-           (iter (cdr t) result))))
-  (iter tree '()))
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (list tree))
+        (else (append (fringe (car tree))
+                      (fringe (cdr tree))))))
+  
 
 (define z (list (list 1 2) (list 3 4)))
 (fringe z)
+
+; Exercise 2.29
+
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+; a)
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (cdr mobile))
+
+(define (branch-length branch)
+  (car branch))
+
+(define (branch-structure branch)
+  (cdr branch))
+
+; b)
+; Return total weight of the mobile
+; This currently calculates the length of a mobile
+; [ ] total-length needs work
+; [ ] total-weight needs work
+
+(define (total-length mobile)
+  (cond ((null? mobile) 0)
+        ((not (pair? mobile)) (branch-length mobile))
+        (else (+ (total-length (left-branch mobile))
+                      (total-length (right-branch mobile))))))
+
+; c)
